b54a76783bc2dcb6ea1da17c61c86bff8ce7c45a
Benjamin Lannon <benjamin@lannonbr.com>
2023-09-27 17:40:54 -0400

    Add clipboard and sum flags

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { parseArgs } from "node:util";
 import { readFile } from "node:fs/promises";
 import pokemon from "pokemontcgsdk";
+import c from "clipboardy";
 
 async function run() {
   const args = parseArgs({
@@ -13,10 +14,16 @@ async function run() {
         type: "string",
         short: "f",
       },
+      sum: {
+        type: "boolean",
+      },
+      clipboard: {
+        type: "boolean",
+      },
     },
   });
 
-  const { file, setName } = args.values;
+  const { file, setName, sum, clipboard } = args.values;
 
   if (typeof file === "undefined" || typeof setName === "undefined") {
     console.error("Error: A set id and file path are required as flags");
@@ -24,6 +31,9 @@ async function run() {
 
     process.exit(1);
   }
+
+  let totalPrice = 0;
+  const cardPrices = [];
 
   pokemon.configure({ apiKey: process.env["POKEMON_TCG_API_TOKEN"] });
 
@@ -35,17 +45,33 @@ async function run() {
   console.log();
 
   for (let cardId of cardIds) {
+    let cardCheapest = 1_000_000; // large number that all prices should relatively be under
+
     const card = await pokemon.card.find(`${setName}-${cardId}`);
 
     const prices = Object.entries(card.tcgplayer.prices).map(
       ([rarity, value]) => {
+        if (value.market < cardCheapest) {
+          cardCheapest = value.market;
+        }
         return `${rarity}: ${value.market}`;
       }
     );
 
+    totalPrice += cardCheapest;
+    cardPrices.push(cardCheapest);
+
     console.log(card.number, card.name, prices);
 
     await sleep(250);
+  }
+
+  if (sum) {
+    console.log("Total:", totalPrice);
+  }
+
+  if (clipboard) {
+    await c.write(cardPrices.join("\n"));
   }
 }
 
@@ -61,6 +87,10 @@ function help() {
   console.log(
     "  --file (-f): file of an array of integers corresponding to card numbers that you wish to look up"
   );
+  console.log(
+    "  --sum: log out the sum of all of the cards with each card's cheapest rarity"
+  );
+  console.log("  --clipboard: post all of the values to the clipboard");
 }
 
 run();
